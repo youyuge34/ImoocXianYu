16bf19adfa9296a307f400facbc1568f40813e9c
Andy <1197993367@qq.com>
2017-05-07 11:18:13 +0800

    5-5完成，首页单图与多图功能完成，使用正常

--- app/src/main/java/com/example/yousheng/imoocxianyu/adapter/CourseAdapter.java
+++ app/src/main/java/com/example/yousheng/imoocxianyu/adapter/CourseAdapter.java
@@ -1,15 +1,24 @@
 package com.example.yousheng.imoocxianyu.adapter;
 
 import android.content.Context;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.yousheng.imoocsdk.imageloader.ImageLoaderManger;
+import com.example.yousheng.imoocsdk.util.Utils;
+import com.example.yousheng.imoocxianyu.R;
 import com.example.yousheng.imoocxianyu.module.recommand.RecommandBodyValue;
 
 import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by yousheng on 17/5/6.
@@ -29,14 +38,14 @@ public class CourseAdapter extends BaseAdapter {
     private LayoutInflater mInflate;
     private Context mContext;
     private ArrayList<RecommandBodyValue> mData;
-
-    private ImageLoaderManger mImageLoaderManger;
+    private ViewHolder mViewHolder;
+    private ImageLoaderManger mImageLoader;
 
     public CourseAdapter(Context mContext, ArrayList<RecommandBodyValue> mData) {
         this.mContext = mContext;
         this.mData = mData;
         mInflate = LayoutInflater.from(mContext);
-        mImageLoaderManger = ImageLoaderManger.getInstance(mContext);
+        mImageLoader = ImageLoaderManger.getInstance(mContext);
     }
 
     @Override
@@ -78,6 +87,128 @@ public class CourseAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        //获取item种类
+        int type = getItemViewType(position);
+        //获取当前位置item的服务器数据
+        RecommandBodyValue value= mData.get(position);
+
+        //若缓存布局为空，则开始新建
+        if (convertView == null) {
+            switch (type) {
+                case CARD_TYPE_ONE:
+                    mViewHolder = new ViewHolder();
+                    convertView = mInflate.inflate(R.layout.item_product_card_one_layout, parent, false);
+                    mViewHolder.mLogoView = (CircleImageView) convertView.findViewById(R.id.item_logo_view);
+                    mViewHolder.mTitleView = (TextView) convertView.findViewById(R.id.item_title_view);
+                    mViewHolder.mInfoView = (TextView) convertView.findViewById(R.id.item_info_view);
+                    mViewHolder.mFooterView = (TextView) convertView.findViewById(R.id.item_footer_view);
+                    mViewHolder.mPriceView = (TextView) convertView.findViewById(R.id.item_price_view);
+                    mViewHolder.mFromView = (TextView) convertView.findViewById(R.id.item_from_view);
+                    mViewHolder.mZanView = (TextView) convertView.findViewById(R.id.item_zan_view);
+                    mViewHolder.mProductLayout = (LinearLayout) convertView.findViewById(R.id.product_photo_layout);
+                    break;
+                case CARD_TYPE_TWO:
+                    mViewHolder = new ViewHolder();
+                    convertView = mInflate.inflate(R.layout.item_product_card_two_layout, parent, false);
+                    mViewHolder.mLogoView = (CircleImageView) convertView.findViewById(R.id.item_logo_view);
+                    mViewHolder.mTitleView = (TextView) convertView.findViewById(R.id.item_title_view);
+                    mViewHolder.mInfoView = (TextView) convertView.findViewById(R.id.item_info_view);
+                    mViewHolder.mFooterView = (TextView) convertView.findViewById(R.id.item_footer_view);
+                    mViewHolder.mProductView = (ImageView) convertView.findViewById(R.id.product_photo_view);
+                    mViewHolder.mPriceView = (TextView) convertView.findViewById(R.id.item_price_view);
+                    mViewHolder.mFromView = (TextView) convertView.findViewById(R.id.item_from_view);
+                    mViewHolder.mZanView = (TextView) convertView.findViewById(R.id.item_zan_view);
+                    break;
+                case CARD_TYPE_THREE:
+                    mViewHolder = new ViewHolder();
+                    break;
+                case VIDOE_TYPE:
+                    mViewHolder = new ViewHolder();
+                    break;
+
+            }
+            convertView.setTag(mViewHolder);
+        } else {
+            mViewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        //开始绑定数据到view层
+        switch (type){
+            case CARD_TYPE_ONE:
+                mImageLoader.displayImage(mViewHolder.mLogoView, value.logo);
+                mViewHolder.mTitleView.setText(value.title);
+                mViewHolder.mInfoView.setText(value.info.concat(mContext.getString(R.string.tian_qian)));
+                mViewHolder.mFooterView.setText(value.text);
+                mViewHolder.mPriceView.setText(value.price);
+                mViewHolder.mFromView.setText(value.from);
+                mViewHolder.mZanView.setText(mContext.getString(R.string.dian_zan).concat(value.zan));
+
+                //因为会布局回收复用，所以要先清空原先的图片布局
+                mViewHolder.mProductLayout.removeAllViews();
+                //动态遍历添加imageview进入水平scrollview中
+                for(String url: value.url){
+                    mViewHolder.mProductLayout.addView(createImageView(url));
+                }
+
+                break;
+            case CARD_TYPE_TWO:
+                mViewHolder.mTitleView.setText(value.title);
+                mViewHolder.mInfoView.setText(value.info.concat(mContext.getString(R.string.tian_qian)));
+                mViewHolder.mFooterView.setText(value.text);
+                mViewHolder.mPriceView.setText(value.price);
+                mViewHolder.mFromView.setText(value.from);
+                mViewHolder.mZanView.setText(mContext.getString(R.string.dian_zan).concat(value.zan));
+                //为单个ImageView加载远程图片
+                mImageLoader.displayImage(mViewHolder.mLogoView, value.logo);
+                mImageLoader.displayImage(mViewHolder.mProductView, value.url.get(0));
+                break;
+            case CARD_TYPE_THREE:
+                break;
+
+        }
+        return convertView;
+    }
+
+    /**
+     * @function 动态代码创建imageView
+     * @param url
+     * @return
+     */
+    private ImageView createImageView(String url) {
+        ImageView imageView = new ImageView(mContext);
+
+        //要与groupview的类型一致。这里是添加到LinearLayout中，所以要用LinearLayout的LayoutParams，若是
+        //用别的layout的param，不会报错，但是特殊的param识别不出
+        LinearLayout.LayoutParams params = new LinearLayout
+                //LayoutParams接收的是a fixed size in pixels，所以需要把dp转成px传入
+                .LayoutParams(Utils.dip2px(mContext,100), ViewGroup.LayoutParams.MATCH_PARENT);
+
+        params.leftMargin= Utils.dip2px(mContext,5);
+        imageView.setLayoutParams(params);
+        mImageLoader.displayImage(imageView,url);
+        return imageView;
+    }
+
+    //用来缓存控件
+    private static class ViewHolder {
+        //所有Card共有属性
+        private CircleImageView mLogoView;
+        private TextView mTitleView;
+        private TextView mInfoView;
+        private TextView mFooterView;
+        //Video Card特有属性
+        private RelativeLayout mVieoContentLayout;
+        private ImageView mShareView;
+
+        //Video Card外所有Card具有属性
+        private TextView mPriceView;
+        private TextView mFromView;
+        private TextView mZanView;
+        //Card One特有属性
+        private LinearLayout mProductLayout;
+        //Card Two特有属性
+        private ImageView mProductView;
+        //Card Three特有属性
+        private ViewPager mViewPager;
     }
 }
